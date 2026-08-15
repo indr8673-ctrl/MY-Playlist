960da34bc7f7b45f3fae6fe13f157269bc60c43a
indr8673-ctrl <indr8673@gmail.com>
2026-08-15 14:39:05 +0600

    Add new TV groups with logos and URLs

--- update_backup.py
+++ update_backup.py
@@ -75,9 +75,24 @@ playlists_to_add = [
         "url": "https://raw.githubusercontent.com/sm-monirulislam/SM-IPTV/refs/heads/main/SM_bdix.m3u"
     },
     {
+        "group_name": "Ayna TV",
+        "group_logo": "https://aynaott.com/assets/images/logo/logo_bg.jpeg",
+        "url": "https://raw.githubusercontent.com/abusaeeidx/Ayna-Playlists-free-Version/refs/heads/main/playlist.m3u"
+    },
+    {
         "group_name": "RoarZone",
         "group_logo": "https://assets.appmeme.com/com.roarzone.tvapps--3-icon.png",
         "url": "https://raw.githubusercontent.com/sm-monirulislam/RoarZone-Auto-Update-playlist/refs/heads/main/RoarZone.m3u"
+    },
+     {
+        "group_name": "Voot",
+        "group_logo": "https://play-lh.googleusercontent.com/InSOp5thAKQxms_ZZfRVjefSQFX2_WDTR1B03C3zcmxftJUkOWC2c__ciwfFLwxT2G6aRQmjfMV28-tnV6dE0w=w480-h960-rw",
+        "url": "https://raw.githubusercontent.com/sm-monirulislam/SM-IPTV/refs/heads/main/vood.m3u"
+    },
+    {
+        "group_name": "Jio Hotstar",
+        "group_logo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQKe_1KwcBrLdVeWT8maozq2ukwcGBXFpxmlnTShnSCErmv5oAXbHVxqaW4&s=10",
+        "url": "https://raw.githubusercontent.com/sm-monirulislam/SM-IPTV/refs/heads/main/jio_hotstar.m3u"
     },
     {
         "group_name": "BDIX",
